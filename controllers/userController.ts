083fed15f408758f8e80683a783545dfd391ea20
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2024-07-11 02:51:19 -0300

    ready for submit

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -2,6 +2,9 @@ import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import { body, validationResult } from "express-validator";
+import { Resend } from "resend";
+
+const TokenGenerator = require("tokgen");
 
 const prisma = new PrismaClient();
 export const getAllUsers = async (req: Request, res: Response) => {
@@ -85,19 +88,34 @@ export const updateUser = [
         return res.status(400).json({ errors: errors.array() });
       }
       const { email_params } = req.params;
-      const { username, email, password } = req.body;
+      const { username } = req.body;
 
-      let hashedPassword;
-      if (password) {
-        hashedPassword = await bcrypt.hash(String(password), 10);
+      try {
+        const user = await prisma.user.findUnique({
+          where: { email: email_params },
+        });
+
+        if (!user) {
+          return res.status(404).json({
+            status: 404,
+            message: "User not found",
+            success: false,
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({
+          status: 500,
+          message: "Internal Server Error",
+          success: false,
+        });
       }
 
       const updatedUser = await prisma.user.update({
         where: { email: email_params },
+
         data: {
           username,
-          email,
-          ...(password && { password: hashedPassword }),
         },
         select: {
           id: true,
@@ -122,3 +140,84 @@ export const updateUser = [
     }
   },
 ];
+
+export const sendTokenPassword = async (req: Request, res: Response) => {
+  let generator = new TokenGenerator({ chars: "0-9a-f", length: 6 });
+  let token = generator.generate();
+  const resend = new Resend(process.env.RESNED_API_KEY);
+  const { email, username } = req.body;
+  const { data, error } = await resend.emails.send({
+    from: "Movix <onboarding@resend.dev>",
+    to: [email],
+    subject: "Password Reset",
+    html: `
+    <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #e0e0e0; border-radius: 10px; background-color: #04152d">
+      <h2 style="color: white; font-weight: bold">Hello ${username} ! </h2>
+      <p style="font-size: 16px; color: white;">
+        We have received a request to reset your password. Please use the following token to change your password:
+      </p>
+      <div style="text-align: center; margin: 20px 0; color: white;">
+        <span style="display: inline-block; padding: 10px 20px; font-size: 18px; color: #fff; background-color: #d9534f;; border-radius: 5px;">${token}</span>
+      </div>
+      <p style="font-size: 16px; color: white;">
+        If you did not request this change, you can ignore this email.
+      </p>
+      <p style="font-size: 16px; color: white;">
+        Thank you,<br>
+        The Movix Team
+      </p>
+      <hr style="border: none; border-top: 1px solid #e0e0e0; margin: 20px 0;">
+      <p style="font-size: 12px; color: white;">
+        If you have any questions, feel free to <a href="mailto:support@movix.com" style="color: #d9534f;">contact us</a>.
+      </p>
+    </div>
+  `,
+  });
+  if (error) {
+    return res.status(400).json({ error });
+  } else {
+    await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        passwordResetToken: token,
+      },
+    });
+    res.status(200).json({ data });
+  }
+};
+
+export const changePassword = async (req: Request, res: Response) => {
+  const { token, password } = req.body;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      passwordResetToken: token,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      status: 400,
+      message: "Invalid token",
+      success: false,
+    });
+  } else {
+    const hashedPassword = await bcrypt.hash(String(password), 10);
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        password: hashedPassword,
+      },
+    });
+
+    res.status(200).json({
+      status: 200,
+      message: "Password updated successfully",
+      success: true,
+    });
+  }
+};
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { protect } from "../controllers/authController";
 import {
+  changePassword,
   getAllUsers,
   getUserById,
+  sendTokenPassword,
   updateUser,
 } from "../controllers/userController";
 import methodNotAllowed from "../middleware/methodNotAllowed";
@@ -12,6 +14,8 @@ export const userRouter = express.Router();
 userRouter.use(protect);
 userRouter.route("/").get(getAllUsers);
 userRouter.route("/:id").get(getUserById);
+userRouter.route("/send-token-password").post(sendTokenPassword);
+userRouter.route("/change-password").put(changePassword);
 userRouter.route("/email/:email_params").put(updateUser);
 userRouter.all("/", methodNotAllowed(["GET"]));
 userRouter.all("/:id", methodNotAllowed(["GET"]));
